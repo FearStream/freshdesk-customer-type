3f7a43278a5713d8906d32c69b93ead2ad639e0e
FearStream <thomas.fear@sharp-stream.com>
2023-09-07 19:26:29 +0100

    script to use on Freshdesk Portal; resolves customer type by search param -> cookie, also setting the cookie; also provides  which gets data for a customer type

---
+++ main.js
@@ -0,0 +1,78 @@
+import Cookies from 'https://esm.run/js-cookie@3.0.5';
+import ENUM from 'https://esm.run/@novaras/js-enum@1.0.3';
+
+/** The possible customer types */
+const CUSTOMER_TYPES = ENUM({
+    BROADCASTER: 'broadcaster',
+    PODCASTER: 'podcaster'
+});
+
+/** The customer type as given in the URL search params. */
+const PARAM_TYPE = (() => {
+    const entry = [...(new URL(window.location.href)).searchParams].find(sp => sp[0] === 'type');
+    return entry && entry[1];
+})();
+
+const COOKIE_KEY = 'customer_type';
+/**
+    @type string
+    The customer type as stored in the cookie.
+    If the cookie doesn't exist, it will be set to `PARAM_TYPE` or `CUSTOMER_TYPES.BROADCASTER`
+*/
+const COOKIE_TYPE = (() => {
+    const cookie_type = Cookies.get(COOKIE_KEY);
+
+    // if no cookie exists, set the cookie
+    if (!cookie_type) {
+        console.log(`No cookie type found; setting`);
+        Cookies.set(COOKIE_KEY, PARAM_TYPE ? PARAM_TYPE : CUSTOMER_TYPES.BROADCASTER);
+
+        // !!TODO!! popup
+    }
+
+    return Cookies.get(COOKIE_KEY);
+})();
+
+/** The resolved customer type. */
+const CUSTOMER_TYPE = PARAM_TYPE ?? COOKIE_TYPE;
+
+/**
+ * Gets the category data for the given customer type, or the type held in `CUSTOMER_TYPE`.
+ * 
+ * The customer articles are filtered & stored on the folders and categories for convenience.
+ *
+ * @param { string } [customer_type]
+ */
+const getCategoriesData = (customer_type = CUSTOMER_TYPE) => {
+    const container = document.getElementById("all-articles");
+    const data = [];
+
+    for (const cat_el of [...container.children]) {
+        const cat = { ...cat_el.dataset, folders: [] };
+        for (const folder_el of [...cat_el.children]) {
+            const folder = { ...folder_el.dataset, articles: [] };
+            for (const article_el of [...folder_el.children]) {
+                const article = { ...article_el.dataset, tags: article_el.dataset.tags.split().filter(tag => tag.length) };
+                folder.articles.push(article);
+            }
+            const customer_articles = folder.articles.filter(article => article.tags.includes(customer_type));
+            folder.customer_articles = customer_articles;
+            cat.folders.push(folder);
+        }
+
+        const customer_folders = cat.folders.filter(folder => folder.customer_articles.length);
+        cat.customer_folders = customer_folders;
+
+        const customer_articles = cat.folders.reduce((articles, folder) => {
+            return [
+                ...articles,
+                ...folder.customer_articles
+            ];
+        }, []);
+        cat.customer_articles = customer_articles;
+
+        data.push(cat);
+    }
+
+    return data;
+};
